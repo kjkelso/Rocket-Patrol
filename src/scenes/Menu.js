5261e61a856465cb1258abf1fb6a91b71cb8d131
Kat Kelso <kjkelso@ucsc.edu>
2020-04-10 16:22:13 -0500

    Scenes & Assets Implemented

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,5 +1,13 @@
-class Menu extends Phaser.scene {
+class Menu extends Phaser.Scene {
     constructor () {
         super ("menuScene");
+    }
+
+    create() {
+        console.log(this);
+        this.add.text(20, 20, "Rocket Patrol Menu");
+
+        // launch the enxt scene
+        this.scene.start("playScene");
     }
 }
