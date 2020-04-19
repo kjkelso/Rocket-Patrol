0d3e7676c59cef3cb616682d3178107851892b30
Kat Kelso <kjkelso@ucsc.edu>
2020-04-19 17:16:03 -0500

    now with comment regarding point breakdown

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
+/*
+Katarina Kelso Point Breakdown
+
+Starting Tier
+Implement the speed increase that happens after 30 seconds in the original game (10)
+Allow the player to control the Rocket after it's fired (10)
+
+Novice Tier
+Display the time remaining (in seconds) on the screen (15)
+Implement parallax scrolling (15)
+
+Intermediate Tier
+Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (25)
+Create new artwork for all of the in-game assets (rocket, spaceships, explosion) (25) */
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
